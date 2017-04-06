fb26e54c50225eb889e2394282f86654af959a75
tmp <tmp@phone-wifi.local>
2017-04-06 13:34:06 +0800

    v0.1.0

---
+++ RCTestLib/RCTestLib.h
@@ -0,0 +1,19 @@
+//
+//  RCTestLib.h
+//  RCTestLib
+//
+//  Created by tmp on 2017/4/6.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RCTestLib.
+FOUNDATION_EXPORT double RCTestLibVersionNumber;
+
+//! Project version string for RCTestLib.
+FOUNDATION_EXPORT const unsigned char RCTestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCTestLib/PublicHeader.h>
+
+
---
+++ RCTestLib/RCTestLib.swift
@@ -0,0 +1,15 @@
+//
+//  RCTestLib.swift
+//  RCTestLib
+//
+//  Created by tmp on 2017/4/6.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+import Foundation
+
+open class RCTestLib {
+    public func showMessage(msg: String) {
+        print("RCTestLib==>>" + msg)
+    }
+}
